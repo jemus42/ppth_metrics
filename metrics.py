299f550d33b15838fce7b2e6162f9a4f38eb8143
Lukas Burk <lukas@quantenbrot.de>
2025-09-14 18:27:40 +0200

    Minor cleanup

--- metrics.py
+++ metrics.py
@@ -90,7 +90,8 @@ class MetricHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write('\n'.join(metrics).encode())
+            # Add trailing newline for POSIX compliance
+            self.wfile.write(('\n'.join(metrics) + '\n').encode())
         else:
             self.send_response(404)
             self.end_headers()
